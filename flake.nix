e88bf93a85e6be8d6b96dbca74e27fca1fa6e745
Harry Ying <lexugeyky@outlook.com>
2021-02-05 17:38:03 +0800

    use nix develop

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "dcompass project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { nixpkgs, rust-overlay, ... }: {
+    devShell."x86_64-linux" = with import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ rust-overlay.overlay ];
+    };
+      mkShell {
+        nativeBuildInputs = [
+          rust-bin.nightly."2021-01-01".rustfmt
+          rust-bin.stable.latest.rust
+          binutils-unwrapped
+        ];
+      };
+  };
+}
