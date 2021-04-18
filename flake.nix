a75284df9d671135ffd5980808537e0fd86eb841
Harry Ying <lexugeyky@outlook.com>
2021-04-18 09:41:43 +0800

    build: fix update app

--- flake.nix
+++ flake.nix
@@ -47,8 +47,8 @@
               wget -O ./data/full.mmdb --show-progress https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
               wget -O ./data/cn.mmdb --show-progress https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb
               wget -O ./data/ipcn.txt --show-progress https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt
-              gzip -k ./data/china.txt
-              gzip -k ./data/ipcn.txt
+              gzip -f -k ./data/china.txt
+              gzip -f -k ./data/ipcn.txt
             '';
         };
         commit = (import ./commit.nix {
